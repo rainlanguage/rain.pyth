39c8fba8d664acf8d28b4f03de49c17ae43ebcef
thedavidmeister <thedavidmeister@gmail.com>
2025-06-09 12:54:48 +0400

    wip on extern

--- src/abstract/PythExtern.sol
+++ src/abstract/PythExtern.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.25;
 
 import {BaseRainterpreterExternNPE2, Operand} from "rain.interpreter/abstract/BaseRainterpreterExternNPE2.sol";
 
+uint256 constant OPCODE_PYTH_PRICE = 0;
+
+uint256 constant OPCODE_FUNCTION_POINTERS_LENGTH = 1;
+
 abstract contract PythExtern is BaseRainterpreterExternNPE2 {
     /// @inheritdoc BaseRainterpreterExternNPE2
     function opcodeFunctionPointers() internal pure override returns (bytes memory) {
@@ -15,5 +19,37 @@ abstract contract PythExtern is BaseRainterpreterExternNPE2 {
         return "";
     }
 
-    // This contract is abstract and does not implement any specific functionality.
+    function buildOpcodeFunctionPointers() external pure returns (bytes memory) {
+        function(Operand, uint256[] memory)
+            internal
+            view
+            returns (uint256[] memory)[] memory fs = new function(Operand, uint256[] memory)
+                internal
+                view
+                returns (uint256[] memory)[](OPCODE_FUNCTION_POINTERS_LENGTH);
+        fs[OPCODE_PYTH_PRICE] = LibOpPythPrice.run;
+
+        uint256[] memory pointers;
+        assembly ("memory-safe") {
+            pointers := fs
+        }
+        return LibConvert.unsafeTo16BitBytes(pointers);
+    }
+
+    function buildIntegrityFunctionPointers() external pure returns (bytes memory) {
+        function(Operand, uint256, uint256)
+            internal
+            pure
+            returns (uint256, uint256)[] memory fs = new function(Operand, uint256, uint256)
+                internal
+                pure
+                returns (uint256, uint256)[](OPCODE_FUNCTION_POINTERS_LENGTH);
+        fs[OPCODE_PYTH_PRICE] = LibOpPythPrice.integrity;
+
+        uint256[] memory pointers;
+        assembly ("memory-safe") {
+            pointers := fs
+        }
+        return LibConvert.unsafeTo16BitBytes(pointers);
+    }
 }
