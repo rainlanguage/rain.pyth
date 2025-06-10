bdad9eb0ff906e9fdaa2db8360b8a7a7940b7c45
thedavidmeister <thedavidmeister@gmail.com>
2025-06-10 11:24:16 +0400

    github action for clean git

---
+++ script/Deploy.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: LicenseRef-DCL-1.0
+// SPDX-FileCopyrightText: Copyright (c) 2020 Rain Open Source Software Ltd
+pragma solidity =0.8.25;
+
+import {Script} from "forge-std/Script.sol";
+import {PythWords} from "../src/concrete/PythWords.sol";
+import {IMetaBoardV1_2} from "rain.metadata/interface/unstable/IMetaBoardV1_2.sol";
+import {LibDescribedByMeta} from "rain.metadata/lib/LibDescribedByMeta.sol";
+
+contract Deploy is Script {
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYMENT_KEY");
+        bytes memory subParserDescribedByMeta = vm.readFileBinary("meta/PythWords.rain.meta");
+        IMetaBoardV1_2 metaboard = IMetaBoardV1_2(vm.envAddress("DEPLOY_METABOARD_ADDRESS"));
+
+        vm.startBroadcast(deployerPrivateKey);
+        PythWords subParser = new PythWords();
+        LibDescribedByMeta.emitForDescribedAddress(metaboard, subParser, subParserDescribedByMeta);
+
+        vm.stopBroadcast();
+    }
+}
