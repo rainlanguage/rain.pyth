e9906b2f910c8f9d2b55077e5ea7de114dee815f
thedavidmeister <thedavidmeister@gmail.com>
2025-06-10 16:45:21 +0400

    i9r commit

--- test/src/lib/pyth/LibPyth.constants.t.sol
+++ test/src/lib/pyth/LibPyth.constants.t.sol
@@ -3,10 +3,22 @@
 pragma solidity =0.8.25;
 
 import {Test} from "forge-std/Test.sol";
+import {LibIntorastring} from "rain.intorastring/lib/LibIntOrAString.sol";
+import {LibPyth} from "src/lib/pyth/LibPyth.sol";
 
 contract LibPythConstantsTest is Test {
-    function testConstants() external {
+    function testChainIdConstants() external {
         assertEq(LibPyth.CHAIN_ID_ARBITRUM, 42161);
         assertEq(LibPyth.CHAIN_ID_BASE, 8453);
+    }
+
+    function testIntorastringConstants() external {
+        assertEq(LibPyth.PRICE_FEED_SYMBOL_INTORASTRING_EQUITY_US_GOOG_USD, LibIntorastring.toString2("Equity.US.GOOG/USD"));
+        assertEq(LibPyth.PRICE_FEED_SYMBOL_INTORASTRING_EQUITY_US_AMZN_USD, LibIntOrAString.toString2("Equity.US.AMZN/USD"));
+        assertEq(LibPyth.PRICE_FEED_SYMBOL_INTORASTRING_EQUITY_US_AAPL_USD, LibIntOrAString.toString2("Equity.US.AAPL/USD"));
+        assertEq(LibPyth.PRICE_FEED_SYMBOL_INTORASTRING_EQUITY_US_MSFT_USD, LibIntOrAString.toString2("Equity.US.MSFT/USD"));
+        assertEq(LibPyth.PRICE_FEED_SYMBOL_INTORASTRING_EQUITY_US_TSLA_USD, LibIntOrAString.toString2("Equity.US.TSLA/USD"));
+        assertEq(LibPyth.PRICE_FEED_SYMBOL_INTORASTRING_EQUITY_US_NVDA_USD, LibIntOrAString.toString2("Equity.US.NVDA/USD"));
+        assertEq(LibPyth.PRICE_FEED_SYMBOL_INTORASTRING_EQUITY_US_META_USD, LibIntOrAString.toString2("Equity.US.META/USD"));
     }
 }
