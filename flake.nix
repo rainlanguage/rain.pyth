e6853323e65cd6e8242d28b2b7f1cf501b832258
thedavidmeister <thedavidmeister@gmail.com>
2025-06-10 20:18:10 +0400

    update meta

--- flake.nix
+++ flake.nix
@@ -3,14 +3,47 @@
 
   inputs = {
     rainix.url = "github:rainprotocol/rainix";
+    rain.url = "github:rainlanguage/rain.cli";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, flake-utils, rainix }:
+  outputs = { self, flake-utils, rainix, rain }:
     flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = rainix.pkgs.${system};
+      in rec
       {
-        packages = rainix.packages.${system};
-        devShells = rainix.devShells.${system};
+        packages = {
+          rain-pyth-prelude = rainix.mkTask.${system} {
+            name = "rain-pyth-prelude";
+            body = ''
+              set -euxo pipefail
+
+              mkdir -p meta;
+              forge script --silent ./script/BuildAuthoringMeta.sol;
+              rain meta build \
+                -i <(cat ./meta/PythFtsoSubParserAuthoringMeta.rain.meta) \
+                -m authoring-meta-v2 \
+                -t cbor \
+                -e deflate \
+                -l none \
+                -o meta/PythFtsoWords.rain.meta \
+                ;
+            '';
+          };
+        } // rainix.packages.${system};
+
+        devShells.default = pkgs.mkShell {
+          packages = [
+            packages.rain-pyth-prelude
+            rain.defaultPackage.${system}
+          ];
+
+          shellHook = rainix.devShells.${system}.default.shellHook;
+          buildInputs = rainix.devShells.${system}.default.buildInputs;
+          nativeBuildInputs = rainix.devShells.${system}.default.nativeBuildInputs;
+
+        };
       }
     );
 
