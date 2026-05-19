4870e8f6d6b9c15a17548cd5d610a06682a1c1f4
Siddharth2207 <siddharthbhoite22@gmail.com>
2026-05-19 12:40:21 +0530

    pointers

--- src/generated/PythWords.pointers.sol
+++ src/generated/PythWords.pointers.sol
@@ -10,7 +10,7 @@ pragma solidity ^0.8.25;
 // file needs the contract to exist so that it can be compiled.
 
 /// @dev Hash of the known bytecode.
-bytes32 constant BYTECODE_HASH = bytes32(0x5d94c45d9a500c4e7faac0ed6cce231d1c5fd68d3a4a2783213dc5ae00c8fe97);
+bytes32 constant BYTECODE_HASH = bytes32(0x722de02276682e53411ffcc00f434daadbc3540bc68d3c26d5582e9b7a4874fb);
 
 /// @dev The hash of the meta that describes the contract.
 bytes32 constant DESCRIBED_BY_META_HASH = bytes32(0xe7bb5842b2cf1d25681a9885109fbf8943495bcebb9ec049bc3790e5db57fa80);
